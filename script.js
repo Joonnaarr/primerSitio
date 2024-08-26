06f7ea467967c5bd8a03cc40153d58b2a9d22699
Jonathan <jonathan.medula@gmail.com>
2024-08-26 09:36:30 -0300

    Se agrego modificacion en body bienvenido

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const name = "Jonathan";
+    const messageElement = document.getElementById("welcome-message");
+    messageElement.textContent = `Hola ${name}, bienvenido`;
+}
+
